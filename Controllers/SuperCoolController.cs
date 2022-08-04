61b1921ffbc47c5ee657ec7c0efe530e2204fa6d
TK888 <Terri.katz88@gmail.com>
2022-08-03 20:18:27 -0400

    startup.cs

---
+++ Controllers/SuperCoolController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NinjaAPI_ForEvolve.Interfaces;
+
+namespace NinjaAPI_ForEvolve.Controllers
+{
+    [Route("v1/[controller]")]
+    public class SuperCoolController : Controller
+    {
+        private readonly IChildService _childService;
+
+        public SuperCoolController(IChildService childService)
+        {
+            _childService = childService ?? throw new ArgumentNullException(nameof(childService));
+        }
+
+        [HttpGet]
+        public void MySuperCoolMethod()
+        {
+            // some code here...
+            _childService.DoSomething();
+            // some more code here...
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,18 @@
+﻿using NinjaAPI_ForEvolve.Interfaces;
+using NinjaAPI_ForEvolve.Services;
+
+namespace NinjaAPI_ForEvolve
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IChildService, ChildService>();
+            services.AddMvc();
+        }
+
+    }
+}
